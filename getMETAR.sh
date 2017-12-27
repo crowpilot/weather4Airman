c5e49c1fc4f46617125b527f4c27cd6d16d3bce4
Toshiaki MATSUYAMA <toshiaki@debian>
2017-12-27 11:53:09 +0900

    metar get

---
+++ getMETAR.sh
@@ -0,0 +1,6 @@
+curl "http://www.imocwx.com/i/metar.php?Area=4&Port=RJNH" > rjnhmetar.html
+curl "http://www.imocwx.com/i/metar.php?Area=4&Port=RJNG" > rjngmetar.html
+curl "http://www.imocwx.com/i/metar.php?Area=4&Port=RJNA" > rjnametar.html
+curl "http://www.imocwx.com/i/metar.php?Area=4&Port=RJNK" > rjnkmetar.html
+curl "http://www.imocwx.com/i/metar.php?Area=2&Port=RJTJ" > rjtjmetar.html
+
